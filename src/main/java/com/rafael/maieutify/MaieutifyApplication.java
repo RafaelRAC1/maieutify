58570334175bf30a8ce30f06a7738ce4ea9563b5
rafael <rafaelcorrea2154r@gmail.com>
2025-01-23 11:28:55 -0300

    git init

---
+++ src/main/java/com/rafael/maieutify/MaieutifyApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.maieutify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaieutifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaieutifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafael/maieutify/MaieutifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.maieutify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaieutifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
