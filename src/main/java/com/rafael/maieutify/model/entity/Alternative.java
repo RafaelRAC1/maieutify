a4f0282172c48e5872f9c7e5fc46db96df35cc02
rafael <rafaelcorrea2154r@gmail.com>
2025-02-02 19:04:41 -0300

    update: remove unused libs

--- src/main/java/com/rafael/maieutify/model/entity/Alternative.java
+++ src/main/java/com/rafael/maieutify/model/entity/Alternative.java
@@ -1,8 +1,5 @@
 package com.rafael.maieutify.model.entity;
 
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
