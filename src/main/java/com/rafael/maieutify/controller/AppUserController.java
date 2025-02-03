541360877ae6b41117dce6a8451ad06f83dd11ec
rafael <rafaelcorrea2154r@gmail.com>
2025-02-03 18:10:42 -0300

    feat: add DTO mapping for retriving data

--- src/main/java/com/rafael/maieutify/controller/AppUserController.java
+++ src/main/java/com/rafael/maieutify/controller/AppUserController.java
@@ -1,16 +1,19 @@
 package com.rafael.maieutify.controller;
 
+import com.rafael.maieutify.mapper.AppUserMapper;
+import com.rafael.maieutify.mapper.dto.user.AppUserDTO;
 import com.rafael.maieutify.model.entity.AppUser;
+import com.rafael.maieutify.service.AppUserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.rafael.maieutify.services.AppUserService;;
+import org.springframework.web.bind.annotation.RestController;;
 
 @RestController
 @RequestMapping("/users")
@@ -19,14 +22,18 @@ public class AppUserController {
     @Autowired
     private AppUserService appUserService;
 
+    @Autowired
+    private AppUserMapper appUserMapper;
+
     @PostMapping("/add")
     public AppUser createUser(@RequestBody AppUser appUser) {
         return appUserService.createUser(appUser);
     }
 
     @GetMapping("/get/{id}")
-    public ResponseEntity<AppUser> getUserById(@PathVariable(value = "id") long userId) throws Exception{
+    public ResponseEntity<AppUserDTO> getUserById(@PathVariable(value = "id") long userId) throws Exception{
         AppUser appUser = appUserService.getUserById(userId);
-        return ResponseEntity.ok().body(appUser);
+        AppUserDTO appUserDTO = this.appUserMapper.appUserToAppUserDTO(appUser);
+        return new ResponseEntity<>(appUserDTO, HttpStatus.OK);
     }
 }
--- src/main/java/com/rafael/maieutify/controller/ListQuestionsController.java
+++ src/main/java/com/rafael/maieutify/controller/ListQuestionsController.java
@@ -1,20 +1,29 @@
 package com.rafael.maieutify.controller;
 
+import java.util.List;
+
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rafael.maieutify.mapper.ListQuestionsCommentMapper;
+import com.rafael.maieutify.mapper.ListQuestionsMapper;
+import com.rafael.maieutify.mapper.dto.list_questions.ListQuestionsDTO;
+import com.rafael.maieutify.mapper.dto.list_questions.ListQuestionsWithQuestionsDTO;
+import com.rafael.maieutify.mapper.dto.list_questions_comments.ListQuestionsCommentDTO;
 import com.rafael.maieutify.model.entity.AppUser;
 import com.rafael.maieutify.model.entity.Category;
 import com.rafael.maieutify.model.entity.ListQuestions;
-import com.rafael.maieutify.services.AppUserService;
-import com.rafael.maieutify.services.CategoryService;
-import com.rafael.maieutify.services.ListQuestionsService;
+import com.rafael.maieutify.service.AppUserService;
+import com.rafael.maieutify.service.CategoryService;
+import com.rafael.maieutify.service.ListQuestionsService;
 
 @RestController
 @RequestMapping("list")
@@ -27,6 +36,12 @@ public class ListQuestionsController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @Autowired
+    private ListQuestionsMapper listQuestionsMapper;
+
+    @Autowired
+    private ListQuestionsCommentMapper listQuestionsCommentMapper;
 
     @PostMapping("add/{category_id}/{user_id}")
     public ResponseEntity<Object> createListQuestions(@PathVariable(value = "category_id") long categoryId,
@@ -44,4 +59,28 @@ public class ListQuestionsController {
         listQuestionsService.createListQuestions(listQuestions);
         return new ResponseEntity<>("List created successfully", HttpStatus.CREATED);
     }
+
+    @GetMapping("get/{list_id}")
+    public ResponseEntity<Object> getListQuestionsById(@PathVariable(value = "list_id") Long listId) {
+        ListQuestions listQuestions = this.listQuestionsService.getListQuestionsById(listId);
+        Hibernate.initialize(listQuestions.getCategory());
+        List<ListQuestionsCommentDTO> comments = this.listQuestionsCommentMapper
+                .listQuestionCommentsToListQuestionCommentDTOs(listQuestions.getListQuestionComment());
+        ListQuestionsDTO listQuestionsDTO = this.listQuestionsMapper.listQuestionsToListQuestionsDTO(listQuestions);
+        listQuestionsDTO.setComments(comments);
+        return new ResponseEntity<>(listQuestionsDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("get-with-questions/{list_id}")
+    public ResponseEntity<Object> getListQuestionsWithQuestionsById(@PathVariable(value = "list_id") Long listId) {
+        ListQuestions listQuestions = this.listQuestionsService.getListQuestionsById(listId);
+        Hibernate.initialize(listQuestions.getCategory());
+        ListQuestionsWithQuestionsDTO listQuestionsWithQuestionsDTO = this.listQuestionsMapper
+                .listQuestionsToListQuestionsWithQuestionsDTO(listQuestions);
+        List<ListQuestionsCommentDTO> comments = this.listQuestionsCommentMapper
+                .listQuestionCommentsToListQuestionCommentDTOs(listQuestions.getListQuestionComment());
+        listQuestionsWithQuestionsDTO.setComments(comments);
+        return new ResponseEntity<>(listQuestionsWithQuestionsDTO, HttpStatus.OK);
+    }
+
 }
