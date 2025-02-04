dc2b518461e29b8af24870ba41c28bb68df7a414
rafael <rafaelcorrea2154r@gmail.com>
2025-02-04 20:24:54 -0300

    refactor: add custom queries with pagination

--- src/main/java/com/rafael/maieutify/repository/ListQuestionsRepository.java
+++ src/main/java/com/rafael/maieutify/repository/ListQuestionsRepository.java
@@ -1,6 +1,19 @@
 package com.rafael.maieutify.repository;
 
+import com.rafael.maieutify.model.entity.AppUser;
+import com.rafael.maieutify.model.entity.Category;
 import com.rafael.maieutify.model.entity.ListQuestions;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ListQuestionsRepository extends JpaRepository<ListQuestions, Long> {}
+public interface ListQuestionsRepository extends JpaRepository<ListQuestions, Long> {
+    List<ListQuestions> findByAppUser(AppUser appUser);
+
+    Page<ListQuestions> findByCategory(Category category, Pageable pageable);
+
+    Page<ListQuestions> findByTitleContaining(String title, Pageable pageable);
+}
--- src/main/java/com/rafael/maieutify/repository/ProfileCommentRepository.java
+++ src/main/java/com/rafael/maieutify/repository/ProfileCommentRepository.java
@@ -1,7 +1,14 @@
 package com.rafael.maieutify.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.rafael.maieutify.model.entity.AppUser;
 import com.rafael.maieutify.model.entity.ProfileComment;
 
-public interface ProfileCommentRepository extends JpaRepository<ProfileComment, Long> {}
+public interface ProfileCommentRepository extends JpaRepository<ProfileComment, Long> {
+    Page<ProfileComment> findByProfile(AppUser profile, Pageable pageable);
+
+    Page<ProfileComment> findByCommenter(AppUser commenter, Pageable pageable);
+}
