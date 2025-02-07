3d5fd254208b4bb79d59b381d264374a25c03aa3
rafael <rafaelcorrea2154r@gmail.com>
2025-02-06 21:22:14 -0300

    feat: add getAll method

--- src/main/java/com/rafael/maieutify/service/CategoryService.java
+++ src/main/java/com/rafael/maieutify/service/CategoryService.java
@@ -1,5 +1,7 @@
 package com.rafael.maieutify.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +23,9 @@ public class CategoryService {
 
     public Iterable<Category> createCategories(Iterable<Category> categories){
         return categoryRepository.saveAll(categories);
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
     }
 }
